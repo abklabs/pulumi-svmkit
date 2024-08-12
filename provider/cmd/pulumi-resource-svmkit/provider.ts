1c9b0508e5c30da6ae78d9c32385285ad2d3387c
Alexander Guy <alexander.guy@pm.me>
2024-08-12 08:00:45 +0100

    Kill off S3 example

--- provider/cmd/pulumi-resource-svmkit/provider.ts
+++ provider/cmd/pulumi-resource-svmkit/provider.ts
@@ -15,8 +15,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as provider from "@pulumi/pulumi/provider";
 
-import { StaticPage, StaticPageArgs } from "./staticPage";
-
 export class Provider implements provider.Provider {
     constructor(readonly version: string, readonly schema: string) { }
 
@@ -25,26 +23,8 @@ export class Provider implements provider.Provider {
 
         // TODO: Add support for additional component resources here.
         switch (type) {
-            case "svmkit:index:StaticPage":
-                return await constructStaticPage(name, inputs, options);
             default:
                 throw new Error(`unknown resource type ${type}`);
         }
     }
-}
-
-async function constructStaticPage(name: string, inputs: pulumi.Inputs,
-    options: pulumi.ComponentResourceOptions): Promise<provider.ConstructResult> {
-
-    // Create the component resource.
-    const staticPage = new StaticPage(name, inputs as StaticPageArgs, options);
-
-    // Return the component resource's URN and outputs as its state.
-    return {
-        urn: staticPage.urn,
-        state: {
-            bucket: staticPage.bucket,
-            websiteUrl: staticPage.websiteUrl,
-        },
-    };
 }
