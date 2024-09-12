5944a54d8cc441477a98a96349d85cd75c739a3e
Kyle Espinola <kyle.s.espinola@gmail.com>
2024-09-12 14:48:03 +0200

    chore: format shell scripts

--- provider/cmd/pulumi-resource-svmkit/assets/lib.bash
+++ provider/cmd/pulumi-resource-svmkit/assets/lib.bash
@@ -1,9 +1,9 @@
-if [ -z "$BASH_VERSION" ] ; then
+if [ -z "$BASH_VERSION" ]; then
     echo "FATAL: This library requires bash to function properly!"
     exit 1
 fi
 
-if [[ ${BASH_VERSINFO[0]} -lt 4 ]] ; then
+if [[ ${BASH_VERSINFO[0]} -lt 4 ]]; then
     echo "FATAL: This library requires bash v4 or greater!"
     exit 1
 fi
@@ -17,23 +17,23 @@ export DEBIAN_FRONTEND
 
 EXIT_FUNCS=()
 
-exit::trap () {
+exit::trap() {
     local func
     for func in "${EXIT_FUNCS[@]}"; do
-	$func
+        $func
     done
 }
 
 trap exit::trap EXIT
 
-exit::trigger () {
+exit::trigger() {
     EXIT_FUNCS+=("$*")
 }
 
 TMPDIR=$(mktemp -d)
 export TMPDIR
 
-temp::cleanup () {
+temp::cleanup() {
     log::debug cleaning up "$TMPDIR"...
     rm -rf "$TMPDIR"
 }
@@ -41,71 +41,81 @@ temp::cleanup () {
 exit::trigger temp::cleanup
 
 # shellcheck disable=SC2120 # these options are optional.
-temp::file () {
+temp::file() {
     mktemp -p "$TMPDIR" "$@"
 }
 
-CRED='' ; CGRN='' ; CYEL='' ; CBLU=''; CNONE=''
+CRED=''
+CGRN=''
+CYEL=''
+CBLU=''
+CNONE=''
 
-if [[ -t 1 ]] ; then
-    CRED='\033[0;31m'; CGRN='\033[0;32m' ;
-    CYEL='\033[0;32m' ; CBLU='\033[0;34m' ; CNONE='\033[0m'
+if [[ -t 1 ]]; then
+    CRED='\033[0;31m'
+    CGRN='\033[0;32m'
+    CYEL='\033[0;32m'
+    CBLU='\033[0;34m'
+    CNONE='\033[0m'
 fi
 
-log::output () {
+log::output() {
     local level
-    level="$1"; shift
+    level="$1"
+    shift
 
     printf "$level:\t%s\n" "$*" >&2
 }
 
-log::debug () {
+log::debug() {
     [[ -v DEBUG ]] || return 0
 
     log::output "${CBLU}DEBUG${CNONE}" "$@"
 }
 
-log::info () {
+log::info() {
     log::output "${CGRN}INFO${CNONE}" "$@"
 }
 
-log::warn () {
+log::warn() {
     log::output "${CYEL}WARN${CNONE}" "$@"
 }
 
-log::error () {
+log::error() {
     log::output "${CRED}ERROR${CNONE}" "$@"
 }
 
-log::fatal () {
+log::fatal() {
     log::output "${CRED}FATAL${CNONE}" "$@"
     exit 1
 }
 
-step::run () {
+step::run() {
     local prefix start name
 
-    prefix=$1 ; shift
+    prefix=$1
+    shift
 
     start=""
 
-    if [[ $# -gt 0 ]] ; then
-	start="setup::$1"; shift
-	log::warn "starting steps with $start..."
+    if [[ $# -gt 0 ]]; then
+        start="setup::$1"
+        shift
+        log::warn "starting steps with $start..."
     fi
 
     while read -r name; do
-	if [[ $name > $start || $name = "$start" ]] ; then
-	    log::info "====> running $name..."
-	    $name
-	fi
+        if [[ $name > $start || $name = "$start" ]]; then
+            log::info "====> running $name..."
+            $name
+        fi
     done < <(declare -F | grep "$prefix" | awk '{ print $3; }')
 }
 
-ssh::config-file () {
+ssh::config-file() {
     local filename
     filename="$(temp::file)"
-    cat > "$filename" <<EOF
+    cat >"$filename" <<EOF
 # This is fine, because we're using public key crypto for our access.
 LogLevel ERROR
 UpdateHostKeys no
@@ -115,44 +125,47 @@ EOF
     echo "$filename"
 }
 
-pulumi::output () {
+pulumi::output() {
     pulumi stack output "$@"
 }
 
-pulumi::config () {
+pulumi::config() {
     pulumi config "$@"
 }
 
-git::version () {
+git::version() {
     git describe --tags --dirty 2>/dev/null || git rev-parse --short HEAD
 }
 
-env::kv () {
-    for varname in "$@" ; do
-	if [[ -v $varname ]] ; then
-	    printf "%s='%s'\n" "$varname" "${!varname}"
-	fi
+env::kv() {
+    for varname in "$@"; do
+        if [[ -v $varname ]]; then
+            printf "%s='%s'\n" "$varname" "${!varname}"
+        fi
     done
 }
 
-env::write () {
+env::write() {
     local var fname mode
 
-    varname=$1 ; shift
-    fname=$1 ; shift
+    varname=$1
+    shift
+    fname=$1
+    shift
 
-    if [[ -v $varname ]] ; then
-	touch "$fname"
+    if [[ -v $varname ]]; then
+        touch "$fname"
 
-	if [[ $# -gt 0 ]] ; then
-	    mode=$1 ; shift
-	    chmod "$mode" "$fname"
-	fi
+        if [[ $# -gt 0 ]]; then
+            mode=$1
+            shift
+            chmod "$mode" "$fname"
+        fi
 
-	printf "%s" "${!varname}" > "$fname"
+        printf "%s" "${!varname}" >"$fname"
     fi
 }
 
-keypair::write () {
+keypair::write() {
     env::write "$@" 600
 }
