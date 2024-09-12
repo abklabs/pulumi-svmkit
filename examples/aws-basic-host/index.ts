780e885c64c524d29ae99d53b8a36b1f06276e27
Kyle Espinola <kyle.s.espinola@gmail.com>
2024-09-12 15:09:12 +0200

    refactor: update the existing examples to work with updated components

--- examples/aws-basic-host/index.ts
+++ examples/aws-basic-host/index.ts
@@ -4,9 +4,11 @@ import * as aws from "@pulumi/aws";
 import * as tls from "@pulumi/tls";
 import * as command from "@pulumi/command";
 
-const config = new pulumi.Config();
+const config = new pulumi.Config("svmkit");
 
-const sshKey = new tls.PrivateKey("sshKey", {
+const ami = config.require("ami");
+
+const sshKey = new tls.PrivateKey("ssh-key", {
     algorithm: "ED25519",
 });
 
@@ -14,68 +16,16 @@ const keyPair = new aws.ec2.KeyPair("keypair", {
     publicKey: sshKey.publicKeyOpenssh,
 });
 
-const validatorKey = new svmkit.KeyPair("validatorKey");
-const voteAccountKey = new svmkit.KeyPair("voteAccount");
+const validatorKey = new svmkit.KeyPair("validator-key");
+const voteAccountKey = new svmkit.KeyPair("vote-account-key");
 
-const validatorConfig = {
-    identityKeyPair: validatorKey.json,
-    voteAccountKeyPair: voteAccountKey.json,
-    entryPoint: [
-        "entrypoint.testnet.solana.com:8001",
-        "entrypoint2.testnet.solana.com:8001",
-        "entrypoint3.testnet.solana.com:8001",
-    ],
-    knownValidator: [
-        "5D1fNXzvv5NjV1ysLjirC4WY92RNsVH18vjmcszZd8on",
-        "7XSY3MrYnK8vq693Rju17bbPkCN3Z7KvvfvJx4kdrsSY",
-        "Ft5fbkqNa76vnsjYNwjDZUXoTWpP7VYm3mtsaQckQADN",
-        "9QxCLckBiJc783jnMvXZubK4wH86Eqqvashtrwvcsgkv",
-    ],
-    expectedGenesisHash: "4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY",
-    useSnapshotArchivesAtStartup: "when-newest",
-    rpcPort: 8899,
-    privateRPC: true,
-    onlyKnownRPC: true,
-    dynamicPortRange: "8002-8020",
-    gossipPort: 8001,
-    rpcBindAddress: "0.0.0.0",
-    walRecoveryMode: "skip_any_corrupted_record",
-    limitLedgerSize: 50000000,
-    blockProductionMethod: "central-scheduler",
-    tvuReceiveThreads: 2,
-    paths: {
-        accounts: "/home/sol/accounts",
-        ledger: "/home/sol/ledger",
-        log: "/home/sol/log",
-    },
+const paths = {
+    accounts: "/home/sol/accounts",
+    ledger: "/home/sol/ledger",
+    log: "/home/sol/log",
 };
 
-const amiIdToUse = (async () => {
-    let v = config.get("instance.amiId");
-
-    if (v) {
-        return v;
-    }
-
-    const amiInfo = await aws.ec2.getAmi({
-        filters: [
-            {
-                name: "name",
-                values: ["debian-12-*"],
-            },
-            {
-                name: "architecture",
-                values: ["x86_64"],
-            },
-        ],
-        owners: ["136693071363"], // Debian
-        mostRecent: true,
-    });
-
-    return amiInfo.id;
-})();
-
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {
+const securityGroup = new aws.ec2.SecurityGroup("security-group", {
     description: "Allow SSH and specific inbound traffic",
     ingress: [
         {
@@ -108,7 +58,7 @@ const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {
 });
 
 const instance = new aws.ec2.Instance("instance", {
-    ami: amiIdToUse,
+    ami,
     instanceType: "t3.large",
     keyName: keyPair.keyName,
     vpcSecurityGroupIds: [securityGroup.id],
@@ -117,17 +67,60 @@ const instance = new aws.ec2.Instance("instance", {
     },
 });
 
-const sshConnInfo: command.types.input.remote.ConnectionArgs = {
+const privateKey = sshKey.privateKeyOpenssh;
+const user = "admin";
+
+const connection: command.types.input.remote.ConnectionArgs = {
     host: instance.publicDns,
-    user: "admin",
-    privateKey: sshKey.privateKeyOpenssh,
+    user,
+    privateKey,
 };
 
-const backendSetup = new svmkit.Backend("backendSetup", {
-    connection: sshConnInfo,
-    triggers: [instance.arn],
-    validatorConfig,
-});
+const ready = new svmkit.Ready(
+    "ready",
+    {
+        connection,
+    },
+    { dependsOn: [instance] },
+);
+
+const backendSetup = new svmkit.Validator(
+    "validator",
+    {
+        connection,
+        flags: {
+            identityKeyPair: validatorKey.json,
+            voteAccountKeyPair: voteAccountKey.json,
+            entryPoint: [
+                "entrypoint.testnet.solana.com:8001",
+                "entrypoint2.testnet.solana.com:8001",
+                "entrypoint3.testnet.solana.com:8001",
+            ],
+            knownValidator: [
+                "5D1fNXzvv5NjV1ysLjirC4WY92RNsVH18vjmcszZd8on",
+                "7XSY3MrYnK8vq693Rju17bbPkCN3Z7KvvfvJx4kdrsSY",
+                "Ft5fbkqNa76vnsjYNwjDZUXoTWpP7VYm3mtsaQckQADN",
+                "9QxCLckBiJc783jnMvXZubK4wH86Eqqvashtrwvcsgkv",
+            ],
+            expectedGenesisHash: "4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY",
+            useSnapshotArchivesAtStartup: "when-newest",
+            rpcPort: 8899,
+            privateRPC: true,
+            onlyKnownRPC: true,
+            dynamicPortRange: "8002-8020",
+            gossipPort: 8001,
+            rpcBindAddress: "0.0.0.0",
+            walRecoveryMode: "skip_any_corrupted_record",
+            limitLedgerSize: 50000000,
+            blockProductionMethod: "central-scheduler",
+            tvuReceiveThreads: 2,
+            paths,
+        },
+    },
+    {
+        dependsOn: [ready],
+    },
+);
 
 export const PUBLIC_DNS_NAME = instance.publicDns;
 export const SSH_PRIVATE_KEY = sshKey.privateKeyOpenssh;
