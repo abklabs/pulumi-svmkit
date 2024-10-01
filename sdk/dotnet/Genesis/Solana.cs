abklabs/svmkit

feat: Port Genesis

sdk/dotnet/Genesis/Solana.cs
@@ -1,0 +1,91 @@
+// *** WARNING: this file was generated by pulumi. ***
+// *** Do not edit by hand unless you're certain you know what you are doing! ***
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Pulumi.Serialization;
+
+namespace Pulumi.Svm.Genesis
+{
+    [SvmResourceType("svm:genesis:Solana")]
+    public partial class Solana : global::Pulumi.CustomResource
+    {
+        [Output("connection")]
+        public Output<Pulumi.Svm.Ssh.Outputs.Connection> Connection { get; private set; } = null!;
+
+        [Output("flags")]
+        public Output<Pulumi.Svm.Solana.Outputs.GenesisFlags> Flags { get; private set; } = null!;
+
+        [Output("genesisHash")]
+        public Output<string> GenesisHash { get; private set; } = null!;
+
+        [Output("primordial")]
+        public Output<ImmutableArray<Outputs.PrimorialEntry>> Primordial { get; private set; } = null!;
+
+
+        /// <summary>
+        /// Create a Solana resource with the given unique name, arguments, and options.
+        /// </summary>
+        ///
+        /// <param name="name">The unique name of the resource</param>
+        /// <param name="args">The arguments used to populate this resource's properties</param>
+        /// <param name="options">A bag of options that control this resource's behavior</param>
+        public Solana(string name, SolanaArgs args, CustomResourceOptions? options = null)
+            : base("svm:genesis:Solana", name, args ?? new SolanaArgs(), MakeResourceOptions(options, ""))
+        {
+        }
+
+        private Solana(string name, Input<string> id, CustomResourceOptions? options = null)
+            : base("svm:genesis:Solana", name, null, MakeResourceOptions(options, id))
+        {
+        }
+
+        private static CustomResourceOptions MakeResourceOptions(CustomResourceOptions? options, Input<string>? id)
+        {
+            var defaultOptions = new CustomResourceOptions
+            {
+                Version = Utilities.Version,
+            };
+            var merged = CustomResourceOptions.Merge(defaultOptions, options);
+            // Override the ID if one was specified for consistency with other language SDKs.
+            merged.Id = id ?? merged.Id;
+            return merged;
+        }
+        /// <summary>
+        /// Get an existing Solana resource's state with the given name, ID, and optional extra
+        /// properties used to qualify the lookup.
+        /// </summary>
+        ///
+        /// <param name="name">The unique name of the resulting resource.</param>
+        /// <param name="id">The unique provider ID of the resource to lookup.</param>
+        /// <param name="options">A bag of options that control this resource's behavior</param>
+        public static Solana Get(string name, Input<string> id, CustomResourceOptions? options = null)
+        {
+            return new Solana(name, id, options);
+        }
+    }
+
+    public sealed class SolanaArgs : global::Pulumi.ResourceArgs
+    {
+        [Input("connection", required: true)]
+        public Input<Pulumi.Svm.Ssh.Inputs.ConnectionArgs> Connection { get; set; } = null!;
+
+        [Input("flags", required: true)]
+        public Input<Pulumi.Svm.Solana.Inputs.GenesisFlagsArgs> Flags { get; set; } = null!;
+
+        [Input("primordial", required: true)]
+        private InputList<Inputs.PrimorialEntryArgs>? _primordial;
+        public InputList<Inputs.PrimorialEntryArgs> Primordial
+        {
+            get => _primordial ?? (_primordial = new InputList<Inputs.PrimorialEntryArgs>());
+            set => _primordial = value;
+        }
+
+        public SolanaArgs()
+        {
+        }
+        public static new SolanaArgs Empty => new SolanaArgs();
+    }
+}

sdk/dotnet/Solana/Inputs/GenesisFlagsArgs.cs
@@ -1,0 +1,53 @@
+// *** WARNING: this file was generated by pulumi. ***
+// *** Do not edit by hand unless you're certain you know what you are doing! ***
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Pulumi.Serialization;
+
+namespace Pulumi.Svm.Solana.Inputs
+{
+
+    public sealed class GenesisFlagsArgs : global::Pulumi.ResourceArgs
+    {
+        [Input("clusterType")]
+        public Input<string>? ClusterType { get; set; }
+
+        [Input("faucetLamports")]
+        public Input<string>? FaucetLamports { get; set; }
+
+        [Input("faucetPubkey", required: true)]
+        public Input<string> FaucetPubkey { get; set; } = null!;
+
+        [Input("identityPubkey", required: true)]
+        public Input<string> IdentityPubkey { get; set; } = null!;
+
+        [Input("inflation")]
+        public Input<string>? Inflation { get; set; }
+
+        [Input("lamportsPerByteYear")]
+        public Input<string>? LamportsPerByteYear { get; set; }
+
+        [Input("ledgerPath", required: true)]
+        public Input<string> LedgerPath { get; set; } = null!;
+
+        [Input("slotPerEpoch")]
+        public Input<string>? SlotPerEpoch { get; set; }
+
+        [Input("stakePubkey", required: true)]
+        public Input<string> StakePubkey { get; set; } = null!;
+
+        [Input("targetLamportsPerSignature")]
+        public Input<string>? TargetLamportsPerSignature { get; set; }
+
+        [Input("votePubkey", required: true)]
+        public Input<string> VotePubkey { get; set; } = null!;
+
+        public GenesisFlagsArgs()
+        {
+        }
+        public static new GenesisFlagsArgs Empty => new GenesisFlagsArgs();
+    }
+}

sdk/dotnet/Solana/Outputs/GenesisFlags.cs
@@ -1,0 +1,65 @@
+// *** WARNING: this file was generated by pulumi. ***
+// *** Do not edit by hand unless you're certain you know what you are doing! ***
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Pulumi.Serialization;
+
+namespace Pulumi.Svm.Solana.Outputs
+{
+
+    [OutputType]
+    public sealed class GenesisFlags
+    {
+        public readonly string? ClusterType;
+        public readonly string? FaucetLamports;
+        public readonly string FaucetPubkey;
+        public readonly string IdentityPubkey;
+        public readonly string? Inflation;
+        public readonly string? LamportsPerByteYear;
+        public readonly string LedgerPath;
+        public readonly string? SlotPerEpoch;
+        public readonly string StakePubkey;
+        public readonly string? TargetLamportsPerSignature;
+        public readonly string VotePubkey;
+
+        [OutputConstructor]
+        private GenesisFlags(
+            string? clusterType,
+
+            string? faucetLamports,
+
+            string faucetPubkey,
+
+            string identityPubkey,
+
+            string? inflation,
+
+            string? lamportsPerByteYear,
+
+            string ledgerPath,
+
+            string? slotPerEpoch,
+
+            string stakePubkey,
+
+            string? targetLamportsPerSignature,
+
+            string votePubkey)
+        {
+            ClusterType = clusterType;
+            FaucetLamports = faucetLamports;
+            FaucetPubkey = faucetPubkey;
+            IdentityPubkey = identityPubkey;
+            Inflation = inflation;
+            LamportsPerByteYear = lamportsPerByteYear;
+            LedgerPath = ledgerPath;
+            SlotPerEpoch = slotPerEpoch;
+            StakePubkey = stakePubkey;
+            TargetLamportsPerSignature = targetLamportsPerSignature;
+            VotePubkey = votePubkey;
+        }
+    }
+}

sdk/nodejs/genesis/solana.ts
@@ -1,0 +1,83 @@
+// *** WARNING: this file was generated by pulumi-language-nodejs. ***
+// *** Do not edit by hand unless you're certain you know what you are doing! ***
+
+import * as pulumi from "@pulumi/pulumi";
+import * as inputs from "../types/input";
+import * as outputs from "../types/output";
+import * as utilities from "../utilities";
+
+export class Solana extends pulumi.CustomResource {
+    /**
+     * Get an existing Solana resource's state with the given name, ID, and optional extra
+     * properties used to qualify the lookup.
+     *
+     * @param name The _unique_ name of the resulting resource.
+     * @param id The _unique_ provider ID of the resource to lookup.
+     * @param opts Optional settings to control the behavior of the CustomResource.
+     */
+    public static get(name: string, id: pulumi.Input<pulumi.ID>, opts?: pulumi.CustomResourceOptions): Solana {
+        return new Solana(name, undefined as any, { ...opts, id: id });
+    }
+
+    /** @internal */
+    public static readonly __pulumiType = 'svm:genesis:Solana';
+
+    /**
+     * Returns true if the given object is an instance of Solana.  This is designed to work even
+     * when multiple copies of the Pulumi SDK have been loaded into the same process.
+     */
+    public static isInstance(obj: any): obj is Solana {
+        if (obj === undefined || obj === null) {
+            return false;
+        }
+        return obj['__pulumiType'] === Solana.__pulumiType;
+    }
+
+    public readonly connection!: pulumi.Output<outputs.ssh.Connection>;
+    public readonly flags!: pulumi.Output<outputs.solana.GenesisFlags>;
+    public /*out*/ readonly genesisHash!: pulumi.Output<string>;
+    public readonly primordial!: pulumi.Output<outputs.genesis.PrimorialEntry[]>;
+
+    /**
+     * Create a Solana resource with the given unique name, arguments, and options.
+     *
+     * @param name The _unique_ name of the resource.
+     * @param args The arguments to use to populate this resource's properties.
+     * @param opts A bag of options that control this resource's behavior.
+     */
+    constructor(name: string, args: SolanaArgs, opts?: pulumi.CustomResourceOptions) {
+        let resourceInputs: pulumi.Inputs = {};
+        opts = opts || {};
+        if (!opts.id) {
+            if ((!args || args.connection === undefined) && !opts.urn) {
+                throw new Error("Missing required property 'connection'");
+            }
+            if ((!args || args.flags === undefined) && !opts.urn) {
+                throw new Error("Missing required property 'flags'");
+            }
+            if ((!args || args.primordial === undefined) && !opts.urn) {
+                throw new Error("Missing required property 'primordial'");
+            }
+            resourceInputs["connection"] = args ? (args.connection ? pulumi.output(args.connection).apply(inputs.ssh.connectionArgsProvideDefaults) : undefined) : undefined;
+            resourceInputs["flags"] = args ? args.flags : undefined;
+            resourceInputs["primordial"] = args ? args.primordial : undefined;
+            resourceInputs["genesisHash"] = undefined /*out*/;
+        } else {
+            resourceInputs["connection"] = undefined /*out*/;
+            resourceInputs["flags"] = undefined /*out*/;
+            resourceInputs["genesisHash"] = undefined /*out*/;
+            resourceInputs["primordial"] = undefined /*out*/;
+        }
+        opts = pulumi.mergeOptions(utilities.resourceOptsDefaults(), opts);
+        super(Solana.__pulumiType, name, resourceInputs, opts);
+    }
+}
+
+/**
+ * The set of arguments for constructing a Solana resource.
+ */
+export interface SolanaArgs {
+    connection: pulumi.Input<inputs.ssh.ConnectionArgs>;
+    flags: pulumi.Input<inputs.solana.GenesisFlagsArgs>;
+    primordial: pulumi.Input<pulumi.Input<inputs.genesis.PrimorialEntryArgs>[]>;
+}

abklabs/pulumi-svmkit

sdk/dotnet/Genesis/Solana.cs
@@ -1,0 +1,91 @@
+// *** WARNING: this file was generated by pulumi. ***
+// *** Do not edit by hand unless you're certain you know what you are doing! ***
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Pulumi.Serialization;
+
+namespace Pulumi.Svmkit.Genesis
+{
+    [SvmkitResourceType("svmkit:genesis:Solana")]
+    public partial class Solana : global::Pulumi.CustomResource
+    {
+        [Output("connection")]
+        public Output<Pulumi.Svmkit.Ssh.Outputs.Connection> Connection { get; private set; } = null!;
+
+        [Output("flags")]
+        public Output<Pulumi.Svmkit.Solana.Outputs.GenesisFlags> Flags { get; private set; } = null!;
+
+        [Output("genesisHash")]
+        public Output<string> GenesisHash { get; private set; } = null!;
+
+        [Output("primordial")]
+        public Output<ImmutableArray<Outputs.PrimorialEntry>> Primordial { get; private set; } = null!;
+
+
+        /// <summary>
+        /// Create a Solana resource with the given unique name, arguments, and options.
+        /// </summary>
+        ///
+        /// <param name="name">The unique name of the resource</param>
+        /// <param name="args">The arguments used to populate this resource's properties</param>
+        /// <param name="options">A bag of options that control this resource's behavior</param>
+        public Solana(string name, SolanaArgs args, CustomResourceOptions? options = null)
+            : base("svmkit:genesis:Solana", name, args ?? new SolanaArgs(), MakeResourceOptions(options, ""))
+        {
+        }
+
+        private Solana(string name, Input<string> id, CustomResourceOptions? options = null)
+            : base("svmkit:genesis:Solana", name, null, MakeResourceOptions(options, id))
+        {
+        }
+
+        private static CustomResourceOptions MakeResourceOptions(CustomResourceOptions? options, Input<string>? id)
+        {
+            var defaultOptions = new CustomResourceOptions
+            {
+                Version = Utilities.Version,
+            };
+            var merged = CustomResourceOptions.Merge(defaultOptions, options);
+            // Override the ID if one was specified for consistency with other language SDKs.
+            merged.Id = id ?? merged.Id;
+            return merged;
+        }
+        /// <summary>
+        /// Get an existing Solana resource's state with the given name, ID, and optional extra
+        /// properties used to qualify the lookup.
+        /// </summary>
+        ///
+        /// <param name="name">The unique name of the resulting resource.</param>
+        /// <param name="id">The unique provider ID of the resource to lookup.</param>
+        /// <param name="options">A bag of options that control this resource's behavior</param>
+        public static Solana Get(string name, Input<string> id, CustomResourceOptions? options = null)
+        {
+            return new Solana(name, id, options);
+        }
+    }
+
+    public sealed class SolanaArgs : global::Pulumi.ResourceArgs
+    {
+        [Input("connection", required: true)]
+        public Input<Pulumi.Svmkit.Ssh.Inputs.ConnectionArgs> Connection { get; set; } = null!;
+
+        [Input("flags", required: true)]
+        public Input<Pulumi.Svmkit.Solana.Inputs.GenesisFlagsArgs> Flags { get; set; } = null!;
+
+        [Input("primordial", required: true)]
+        private InputList<Inputs.PrimorialEntryArgs>? _primordial;
+        public InputList<Inputs.PrimorialEntryArgs> Primordial
+        {
+            get => _primordial ?? (_primordial = new InputList<Inputs.PrimorialEntryArgs>());
+            set => _primordial = value;
+        }
+
+        public SolanaArgs()
+        {
+        }
+        public static new SolanaArgs Empty => new SolanaArgs();
+    }
+}

sdk/dotnet/Solana/Inputs/GenesisFlagsArgs.cs
@@ -1,0 +1,53 @@
+// *** WARNING: this file was generated by pulumi. ***
+// *** Do not edit by hand unless you're certain you know what you are doing! ***
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Pulumi.Serialization;
+
+namespace Pulumi.Svmkit.Solana.Inputs
+{
+
+    public sealed class GenesisFlagsArgs : global::Pulumi.ResourceArgs
+    {
+        [Input("clusterType")]
+        public Input<string>? ClusterType { get; set; }
+
+        [Input("faucetLamports")]
+        public Input<string>? FaucetLamports { get; set; }
+
+        [Input("faucetPubkey", required: true)]
+        public Input<string> FaucetPubkey { get; set; } = null!;
+
+        [Input("identityPubkey", required: true)]
+        public Input<string> IdentityPubkey { get; set; } = null!;
+
+        [Input("inflation")]
+        public Input<string>? Inflation { get; set; }
+
+        [Input("lamportsPerByteYear")]
+        public Input<string>? LamportsPerByteYear { get; set; }
+
+        [Input("ledgerPath", required: true)]
+        public Input<string> LedgerPath { get; set; } = null!;
+
+        [Input("slotPerEpoch")]
+        public Input<string>? SlotPerEpoch { get; set; }
+
+        [Input("stakePubkey", required: true)]
+        public Input<string> StakePubkey { get; set; } = null!;
+
+        [Input("targetLamportsPerSignature")]
+        public Input<string>? TargetLamportsPerSignature { get; set; }
+
+        [Input("votePubkey", required: true)]
+        public Input<string> VotePubkey { get; set; } = null!;
+
+        public GenesisFlagsArgs()
+        {
+        }
+        public static new GenesisFlagsArgs Empty => new GenesisFlagsArgs();
+    }
+}

sdk/dotnet/Solana/Outputs/GenesisFlags.cs
@@ -1,0 +1,65 @@
+// *** WARNING: this file was generated by pulumi. ***
+// *** Do not edit by hand unless you're certain you know what you are doing! ***
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Pulumi.Serialization;
+
+namespace Pulumi.Svmkit.Solana.Outputs
+{
+
+    [OutputType]
+    public sealed class GenesisFlags
+    {
+        public readonly string? ClusterType;
+        public readonly string? FaucetLamports;
+        public readonly string FaucetPubkey;
+        public readonly string IdentityPubkey;
+        public readonly string? Inflation;
+        public readonly string? LamportsPerByteYear;
+        public readonly string LedgerPath;
+        public readonly string? SlotPerEpoch;
+        public readonly string StakePubkey;
+        public readonly string? TargetLamportsPerSignature;
+        public readonly string VotePubkey;
+
+        [OutputConstructor]
+        private GenesisFlags(
+            string? clusterType,
+
+            string? faucetLamports,
+
+            string faucetPubkey,
+
+            string identityPubkey,
+
+            string? inflation,
+
+            string? lamportsPerByteYear,
+
+            string ledgerPath,
+
+            string? slotPerEpoch,
+
+            string stakePubkey,
+
+            string? targetLamportsPerSignature,
+
+            string votePubkey)
+        {
+            ClusterType = clusterType;
+            FaucetLamports = faucetLamports;
+            FaucetPubkey = faucetPubkey;
+            IdentityPubkey = identityPubkey;
+            Inflation = inflation;
+            LamportsPerByteYear = lamportsPerByteYear;
+            LedgerPath = ledgerPath;
+            SlotPerEpoch = slotPerEpoch;
+            StakePubkey = stakePubkey;
+            TargetLamportsPerSignature = targetLamportsPerSignature;
+            VotePubkey = votePubkey;
+        }
+    }
+}

sdk/nodejs/genesis/solana.ts
@@ -1,0 +1,83 @@
+// *** WARNING: this file was generated by pulumi-language-nodejs. ***
+// *** Do not edit by hand unless you're certain you know what you are doing! ***
+
+import * as pulumi from "@pulumi/pulumi";
+import * as inputs from "../types/input";
+import * as outputs from "../types/output";
+import * as utilities from "../utilities";
+
+export class Solana extends pulumi.CustomResource {
+    /**
+     * Get an existing Solana resource's state with the given name, ID, and optional extra
+     * properties used to qualify the lookup.
+     *
+     * @param name The _unique_ name of the resulting resource.
+     * @param id The _unique_ provider ID of the resource to lookup.
+     * @param opts Optional settings to control the behavior of the CustomResource.
+     */
+    public static get(name: string, id: pulumi.Input<pulumi.ID>, opts?: pulumi.CustomResourceOptions): Solana {
+        return new Solana(name, undefined as any, { ...opts, id: id });
+    }
+
+    /** @internal */
+    public static readonly __pulumiType = 'svmkit:genesis:Solana';
+
+    /**
+     * Returns true if the given object is an instance of Solana.  This is designed to work even
+     * when multiple copies of the Pulumi SDK have been loaded into the same process.
+     */
+    public static isInstance(obj: any): obj is Solana {
+        if (obj === undefined || obj === null) {
+            return false;
+        }
+        return obj['__pulumiType'] === Solana.__pulumiType;
+    }
+
+    public readonly connection!: pulumi.Output<outputs.ssh.Connection>;
+    public readonly flags!: pulumi.Output<outputs.solana.GenesisFlags>;
+    public /*out*/ readonly genesisHash!: pulumi.Output<string>;
+    public readonly primordial!: pulumi.Output<outputs.genesis.PrimorialEntry[]>;
+
+    /**
+     * Create a Solana resource with the given unique name, arguments, and options.
+     *
+     * @param name The _unique_ name of the resource.
+     * @param args The arguments to use to populate this resource's properties.
+     * @param opts A bag of options that control this resource's behavior.
+     */
+    constructor(name: string, args: SolanaArgs, opts?: pulumi.CustomResourceOptions) {
+        let resourceInputs: pulumi.Inputs = {};
+        opts = opts || {};
+        if (!opts.id) {
+            if ((!args || args.connection === undefined) && !opts.urn) {
+                throw new Error("Missing required property 'connection'");
+            }
+            if ((!args || args.flags === undefined) && !opts.urn) {
+                throw new Error("Missing required property 'flags'");
+            }
+            if ((!args || args.primordial === undefined) && !opts.urn) {
+                throw new Error("Missing required property 'primordial'");
+            }
+            resourceInputs["connection"] = args ? (args.connection ? pulumi.output(args.connection).apply(inputs.ssh.connectionArgsProvideDefaults) : undefined) : undefined;
+            resourceInputs["flags"] = args ? args.flags : undefined;
+            resourceInputs["primordial"] = args ? args.primordial : undefined;
+            resourceInputs["genesisHash"] = undefined /*out*/;
+        } else {
+            resourceInputs["connection"] = undefined /*out*/;
+            resourceInputs["flags"] = undefined /*out*/;
+            resourceInputs["genesisHash"] = undefined /*out*/;
+            resourceInputs["primordial"] = undefined /*out*/;
+        }
+        opts = pulumi.mergeOptions(utilities.resourceOptsDefaults(), opts);
+        super(Solana.__pulumiType, name, resourceInputs, opts);
+    }
+}
+
+/**
+ * The set of arguments for constructing a Solana resource.
+ */
+export interface SolanaArgs {
+    connection: pulumi.Input<inputs.ssh.ConnectionArgs>;
+    flags: pulumi.Input<inputs.solana.GenesisFlagsArgs>;
+    primordial: pulumi.Input<pulumi.Input<inputs.genesis.PrimorialEntryArgs>[]>;
+}