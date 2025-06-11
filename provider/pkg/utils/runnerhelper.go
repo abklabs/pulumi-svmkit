03aa9a74aef1a5a8f73c868431e609d483970607
Dante DeLucia <dante@abklabs.com>
2025-06-11 12:03:08 -0700

    Add ProgressCallback

--- provider/pkg/utils/runnerhelper.go
+++ provider/pkg/utils/runnerhelper.go
@@ -3,9 +3,10 @@ package utils
 import (
 	"context"
 	"fmt"
-
+	"time"
 	"github.com/abklabs/pulumi-svmkit/pkg/ssh"
 	"github.com/abklabs/svmkit/pkg/runner"
+	p "github.com/pulumi/pulumi-go-provider"
 )
 
 type RunnerArgs struct {
@@ -23,11 +24,36 @@ func RunnerHelper(ctx context.Context, runnerArgs RunnerArgs, command runner.Com
 		return fmt.Errorf("failed to dial SSH connection to hosst: %w", err)
 	}
 
+
+	pcb := func(filename string, copied int, size int, start time.Time) {
+			logger := p.GetLogger(ctx)
+			elapsed := time.Since(start).Seconds()
+			speed := float64(copied) / elapsed // bytes/sec
+			if size != 0 {
+				msg := "%s: %2.2d%% (%d bytes - %0.3f MB/s)\n"
+				logger.InfoStatus(
+					fmt.Sprintf(
+						msg,
+						filename,
+						100 * copied/size,
+						copied,
+						speed/1024/1024))
+			} else {
+				msg := "%s: (%d bytes - %0.3f MB/s)\n"
+				logger.InfoStatus(
+					fmt.Sprintf(
+						msg,
+						filename,
+						copied,
+						speed/1024/1024))
+			}
+		}
+	
 	r := runner.NewRunner(client, command)
 
 	handler := MakePulumiLogger(ctx)
 
-	if err := r.Run(ctx, handler); err != nil {
+	if err := r.Run(ctx, handler, pcb); err != nil {
 		return err
 	}
 
