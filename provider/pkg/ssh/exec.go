ba31021687bf084a2179bb34e3ee007d806671d7
Dante DeLucia <dante@abklabs.com>
2025-06-25 11:26:20 -0700

    Satisfy lint checks

--- provider/pkg/ssh/exec.go
+++ provider/pkg/ssh/exec.go
@@ -3,26 +3,36 @@ package ssh
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 
 	"golang.org/x/crypto/ssh"
 )
 
-func Exec(ctx context.Context, client *ssh.Client, command string) (string, string, error) {
+func Exec(ctx context.Context, client *ssh.Client, command string) (stdout string, stderr string, err error) {
 	// Create a new session for running the command
-	execSession, err := client.NewSession()
+
+	var execSession *ssh.Session
+	execSession, err = client.NewSession()
 	if err != nil {
 		return "", "", fmt.Errorf("failed to create SSH session: %w", err)
 	}
-	defer execSession.Close()
 
-	stdoutPipe, err := execSession.StdoutPipe()
+	defer func() {
+		if closeErr := execSession.Close(); closeErr != io.EOF {
+			err = errors.Join(closeErr)
+		}
+	}()
+
+	var stdoutPipe io.Reader
+	stdoutPipe, err = execSession.StdoutPipe()
 	if err != nil {
 		return "", "", fmt.Errorf("failed to get stdout pipe: %w", err)
 	}
 
-	stderrPipe, err := execSession.StderrPipe()
+	var stderrPipe io.Reader
+	stderrPipe, err = execSession.StderrPipe()
 	if err != nil {
 		return "", "", fmt.Errorf("failed to get stderr pipe: %w", err)
 	}
@@ -32,25 +42,25 @@ func Exec(ctx context.Context, client *ssh.Client, command string) (string, stri
 	stderrDone := make(chan struct{})
 
 	go func() {
-		if _, err := io.Copy(&stdoutBuf, stdoutPipe); err != nil {
+		if _, err = io.Copy(&stdoutBuf, stdoutPipe); err != nil {
 			fmt.Printf("error copying stdout: %v\n", err)
 		}
 		close(stdoutDone)
 	}()
 
 	go func() {
-		if _, err := io.Copy(&stderrBuf, stderrPipe); err != nil {
+		if _, err = io.Copy(&stderrBuf, stderrPipe); err != nil {
 			fmt.Printf("error copying stderr: %v\n", err)
 		}
 		close(stderrDone)
 	}()
 
 	bashCommand := fmt.Sprintf("/bin/sh -c '%s'", command)
-	if err := execSession.Start(bashCommand); err != nil {
+	if err = execSession.Start(bashCommand); err != nil {
 		return "", "", fmt.Errorf("failed to start command: %w", err)
 	}
 
-	if err := execSession.Wait(); err != nil {
+	if err = execSession.Wait(); err != nil {
 		return "", "", fmt.Errorf("command execution failed: %w", err)
 	}
 
