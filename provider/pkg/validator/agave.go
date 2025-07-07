d8444f079f580997a10f3f818eda0f2e3f74a146
Alexander Guy <alexander.guy@pm.me>
2025-07-07 09:56:33 -0700

    Kill off these noisy comments

--- provider/pkg/validator/agave.go
+++ provider/pkg/validator/agave.go
@@ -8,34 +8,18 @@ import (
 	"github.com/abklabs/svmkit/pkg/validator"
 )
 
-// Agave represents a Pulumi resource for managing an Agave validator.
 type Agave struct{}
 
-// AgaveArgs represents the input arguments required to create an Agave resource.
 type AgaveArgs struct {
 	utils.RunnerArgs
 	agave.Agave
 }
 
-// AgaveState represents the state of an Agave resource.
 type AgaveState struct {
 	AgaveArgs
 	validator.Properties
 }
 
-// Create is the method that Pulumi calls to create an Agave resource.
-// It sets up the Agave validator on the specified machine using the provided connection and flags.
-//
-// Parameters:
-// - ctx: The context for the creation operation.
-// - name: The name of the resource.
-// - input: The input arguments for creating the resource.
-// - preview: A boolean indicating whether this is a preview operation.
-//
-// Returns:
-// - The name of the created resource.
-// - The state of the created resource.
-// - An error if the creation fails.
 func (Agave) Create(ctx context.Context, name string, input AgaveArgs, preview bool) (string, AgaveState, error) {
 	state := AgaveState{AgaveArgs: input}
 
@@ -56,20 +40,6 @@ func (Agave) Create(ctx context.Context, name string, input AgaveArgs, preview b
 	return name, state, nil
 }
 
-// Update is the method that Pulumi calls to update an Agave resource.
-// It sets up the Agave validator on the specified machine using the provided connection and flags.
-//
-// Parameters:
-// - ctx: The context for the update operation.
-// - name: The name of the resource.
-// - oldInput: The previous input arguments for the resource.
-// - newInput: The new input arguments for updating the resource.
-// - preview: A boolean indicating whether this is a preview operation.
-//
-// Returns:
-// - The name of the updated resource.
-// - The state of the updated resource.
-// - An error if the update fails.
 func (Agave) Update(ctx context.Context, name string, oldInput, newInput AgaveArgs, preview bool) (string, AgaveState, error) {
 	state := AgaveState{AgaveArgs: newInput}
 
